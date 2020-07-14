ec4149a77e0d04575e6299e473e3de7ff7977580
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:24:15 -0400

    exercise 4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,15 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  try {
+    const response = await request({
+      uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+      headers: { Accept: "application/json" },
+    });
+    const geekJoke = JSON.parse(response);
+    return geekJoke.joke;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
 };
+getGeekJoke().then((data) => console.log(data));
