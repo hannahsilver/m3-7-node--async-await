bf4b3a2ecc4900693d73aa34d514f2d0ee6f3223
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:23:24 -0400

    exercise 3

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -1,12 +1,26 @@
 // Exercise 3
 // ----------
 
+const { response } = require("express");
+
 const doublesLater = (num) => {
   // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
   // 2. returns the double of the num
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 2000);
+  });
 };
 
 // 3. handleSum function (async/await)
 
-// 4. verification
+const handleSum = async (num) => {
+  const a = await doublesLater(num);
+  const b = await doublesLater(num);
+  const c = await doublesLater(num);
+  return a + b + c;
+};
+
+// // 4. verification
 handleSum(10).then((ans) => console.log(ans));
