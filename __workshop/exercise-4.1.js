f35138112fc738fbbba4d412faeed157815946c0
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:23:37 -0400

    exercise 4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,17 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
-// getDadJoke
+const getDadJoke = async () => {
+  try {
+    const response = await request({
+      uri: "https://icanhazdadjoke.com",
+      headers: { Accept: "application/json" },
+    });
+    const dadJoke = JSON.parse(response);
+    return dadJoke.joke;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
 
 // 4.1
-// getJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
