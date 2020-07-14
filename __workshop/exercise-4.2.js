dc6cc8ba91af2f44dd67447229403d95f3247d85
Hannah Silver <hannah_silver@hotmail.com>
2020-07-13 23:23:58 -0400

    exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,16 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    const response = await request({
+      uri: "https://api.tronalddump.io/random/quote",
+      headers: { Accept: "application/json" },
+    });
+    const dumpQuote = JSON.parse(response);
+    return dumpQuote.value;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
 };
+
+getTronaldDumpQuote().then((data) => console.log(data));
